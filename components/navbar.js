db424ff7115a45e1009c072170f1a1f3a46ae0c1
achtsnits <stefan.achtsnit@eox.at>
2024-01-25 07:49:23 +0100

    add placeholder for privacy notice

--- components/navbar.js
+++ components/navbar.js
@@ -71,7 +71,7 @@ customElements.define(
                                   this.getAttribute("enable-login") !== null
                                     ? `
                                     <li class="l-list__item u-display-none u-display-md-block">
-                                        <a class="c-button c-button--primary c-button--negative c-button--sm c-header__account" href="/login">
+                                        <a class="c-button c-button--primary c-button--negative c-button--sm c-header__account" href="https://share-eu1.hsforms.com/1FHWah5_qRoKHvJBqDsJRoQ2dqt8d">
                                             <svg style="width: 1em" class="svg-inline--fa fa-sign-in fa-w-16" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sign-in" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
                                                 <path fill="#003247" d="M137.2 110.3l21.9-21.9c9.3-9.3 24.5-9.4 33.9-.1L344.9 239c9.5 9.4 9.5 24.7 0 34.1L193 423.7c-9.4 9.3-24.5 9.3-33.9-.1l-21.9-21.9c-9.7-9.7-9.3-25.4.8-34.7l77.6-71.1H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h191.5l-77.6-71.1c-10-9.1-10.4-24.9-.7-34.5zM512 352V160c0-53-43-96-96-96h-84c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h84c17.7 0 32 14.3 32 32v192c0 17.7-14.3 32-32 32h-84c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h84c53 0 96-43 96-96z"></path>
                                             </svg>
